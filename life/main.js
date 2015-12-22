3d81ec89f8fe3465463665c1640ef0f15f5939dc
brian_ray <monodesigns@gmail.com>
2015-12-22 17:11:59 -0500

    Create your own shapes
    Added functionality to create your own shapes and click the run button
    to start.

--- life/main.js
+++ life/main.js
@@ -25,19 +25,28 @@ for(var x = 0; x < xLimit; x++) {
 	}
 }
 
-var glider = new Array('x2y1', 'x3y2', 'x1y3', 'x2y3', 'x3y3');
-var length = glider.length;
-for(var i = 0; i < length; i++) {
-	grid[glider[i]] = 1;
+$('td').on('click', function(){
+	var cellClass = $(this).attr('class');
+	grid[cellClass] = grid[cellClass] == 1 ? 0 : 1;
+	$(this).toggleClass('active');
+});
+
+// var shape = new Array('x2y1', 'x3y2', 'x1y3', 'x2y3', 'x3y3'); // glider
+// var shape = new Array('x25y24','x24y25','x25y25','x26y25','x25y26'); // flashing star
+// var length = shape.length;
+// for(var i = 0; i < length; i++) {
+// 	grid[shape[i]] = 1;
+// }
+
+// process(grid);
+// display(grid);
+
+function run() {
+	func = setInterval(function(){
+		grid = process(grid);
+		display(grid);
+	},100);
 }
-
-process(grid);
-display(grid);
-
-func = setInterval(function(){
-	grid = process(grid);
-	display(grid);
-},100);
 
 function process(grid) {
 	var newGrid = new Array();
